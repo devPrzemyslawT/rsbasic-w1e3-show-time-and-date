fb48695ac967a679376a7e305b24ccd47d1c53d2
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-02-22 06:32:03 +0100

    script now created - to run app

--- time.js
+++ time.js
@@ -1,1 +1,6 @@
+var moment = require("moment"); // require
+
+console.log("Date object:");
 console.log(Date());
+console.log("Moment library: ");
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
